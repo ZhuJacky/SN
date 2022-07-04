63ccf9111bca320a1a2191e7cd75b84af1668f61
unknown <yonghui.zhu@narwal.com>
2022-07-04 18:16:09 +0800

    update

--- gin/README.md
+++
@@ -1,5 +0,0 @@
-# SN
-maintainer: 永辉
-
-此代码对应文章：Golang 简洁架构实战 https://www.luozhiyun.com/archives/640
-
--- gin/app/gin_engine.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import "github.com/gin-gonic/gin"
-
-func NewGinEngine() *gin.Engine {
-	return gin.Default()
-}
